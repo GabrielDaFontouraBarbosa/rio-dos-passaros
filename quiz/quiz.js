2a1afdaa448917ed376ae7745f25b378738c0355
n0tzena <zenazathoth@gmail.com>
2026-07-19 20:43:27 -0300

    quiz funcionando

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -5,7 +5,26 @@ const respostaB = document.getElementById("respostaB");
 const respostaC = document.getElementById("respostaC");
 const respostaD = document.getElementById("respostaD");
 
+const divRA = document.getElementById("divRA")
+const divRB = document.getElementById("divRB")
+const divRC = document.getElementById("divRC")
+const divRD = document.getElementById("divRD")
 
+divRA.addEventListener("click", (e) => {
+    responderPergunta("a")
+})
+divRB.addEventListener("click", (e) => {
+    responderPergunta("b")
+})
+divRC.addEventListener("click", (e) => {
+    responderPergunta("c")
+})
+divRD.addEventListener("click", (e) => {
+    responderPergunta("d")
+})
+
+
+var alternativaCorreta;
 var acertos = 0;
 var erros = 0;
 
@@ -37,13 +56,64 @@ function adicionarSegundo()
 
 function carregarPergunta()
 {
-    var pergunta = perguntas[getRandomInt(0, perguntas.length)]
+    var randomNumber = getRandomInt(0, perguntas.length)
+    var pergunta = perguntas[randomNumber]
+    perguntas.splice(randomNumber, 1)
 
     perguntaElement.innerText = pergunta.pergunta
     respostaA.innerText = pergunta.respostas.a.resposta
     respostaB.innerText = pergunta.respostas.b.resposta
     respostaC.innerText = pergunta.respostas.c.resposta
     respostaD.innerText = pergunta.respostas.d.resposta
+
+    definirAlternativaCorreta(pergunta)
+}
+function definirAlternativaCorreta(pergunta)
+{
+    if(pergunta.respostas.a.correta)
+    {
+        alternativaCorreta = "a"
+    }
+    else if(pergunta.respostas.b.correta)
+    {
+        alternativaCorreta = "b"
+    }
+    else if(pergunta.respostas.c.correta)
+    {
+        alternativaCorreta = "c"
+    }
+    else if(pergunta.respostas.d.correta)
+    {
+        alternativaCorreta = "d"
+    }
+}
+function responderPergunta(resposta)
+{
+    if (resposta === alternativaCorreta)
+    {
+        acertos++
+    }
+    else
+    {
+        erros++
+    }
+
+    if (perguntas.length === 0)
+    {
+        finalizarQuiz()
+        return
+    }
+
+    carregarPergunta();
+}
+
+function finalizarQuiz()
+{
+    clearInterval(timer)
+    sessionStorage.setItem('quiz_acertos', acertos)
+    sessionStorage.setItem('quiz_erros', erros)
+    sessionStorage.setItem('quiz_tempo', tempo)
+    window.location.href = 'final.html'
 }
 
 iniciarTimer();
